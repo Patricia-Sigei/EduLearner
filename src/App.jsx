64e8b83d671739bf4540715897940a35985083a7
Patricia-Sigei <pattychela97@gmail.com>
2025-01-18 10:58:40 +0300

    improves layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import Navbar from './Components/NavBar/Navbar.'
-import UserLogIn from './Components/LogIn/UserLogIn'
-import Home from './Components/Home/Home'
-import Assignments from './Components/Assignments/Assignments'
-import Lessons from './Components/Lessons/Lessons'
-import SideBar from './Components/SideBar/SideBar'
-import Resources from './Components/Resources/Resources'
+import React from 'react';
+import Navbar from './Components/NavBar/Navbar';
+import UserLogIn from './Components/LogIn/UserLogIn';
+import Home from './Components/Home/Home';
+import Assignments from './Components/Assignments/Assignments';
+import Lessons from './Components/Lessons/Lessons';
+import SideBar from './Components/SideBar/SideBar';
+import Resources from './Components/Resources/Resources';
+import './App.css';
 
 const App = () => {
   return (
-    <div> 
-      <Navbar/>
-      <SideBar/>
-      <UserLogIn/>
-      <Home/>
-      <Lessons/>
-      <Assignments/>
-      <Resources/>
+    <div className="app-layout">
+      <div className="sidebar">
+        <SideBar />
+      </div>
+      <div className="main-content">
+        <Navbar />
+        <div className="page-content">
+          <UserLogIn />
+          <Home />
+          <Lessons />
+          <Assignments />
+          <Resources />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
